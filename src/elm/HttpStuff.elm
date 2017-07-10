b2e674b017405538dff933df91053461fdccbf23
Matti Pohjanvirta <matti.pohjanvirta@iki.fi>
2017-07-10 15:03:29 +0300

    use https links

--- src/elm/HttpStuff.elm
+++ src/elm/HttpStuff.elm
@@ -11,20 +11,20 @@ getOpenAlkoInfo =
     let
         url =
           --"https://gist.githubusercontent.com/Grrandi/4a819a79ee4ab3d5b75a6f08a826fa96/raw/8a93ec5557f21fe510e81300c9a07ea8c4d4f617/object.json"
-          "http://oaa-server.onkoalkoauki.fi/open_stores"
+          "https://oaa-server.onkoalkoauki.fi/open_stores"
     in
         Http.send GotInfo fuufaa
 
 
 fuufaa : Http.Request (List StoreInfos)
 fuufaa =
-    Http.get "http://oaa-server.onkoalkoauki.fi/open_stores" decodeStores
+    Http.get "https://oaa-server.onkoalkoauki.fi/open_stores" decodeStores
 
 getOpenAlkos : Cmd Msg
 getOpenAlkos =
   let
     url =
       --"https://gist.githubusercontent.com/Grrandi/4a819a79ee4ab3d5b75a6f08a826fa96/raw/47624ba7c1eb09487139676f64789417cfeceece/.json"
-      "http://localhost:5000/open_store_count"
+      "https://oaa-server.onkoalkoauki.fi/open_store_count"
   in
     Http.send GotCount (Http.get url decodeCount)
